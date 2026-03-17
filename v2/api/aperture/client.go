a08863bc3ebc66c7eda089f725b67420c41bea3d
II <picus.2022@mail.ru>
2026-03-17 16:23:04 +0300

    X

--- v2/api/aperture/client.go
+++ v2/api/aperture/client.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/goaperture/goaperture/v2/api/auth"
 	"github.com/goaperture/goaperture/v2/api/client"
+	"github.com/goaperture/goaperture/v2/exception"
 )
 
 func (a *Api[P]) GetClientPayload(ctx context.Context) *P {
@@ -18,5 +19,15 @@ func (a *Api[P]) GetClientPayload(ctx context.Context) *P {
 	}
 
 	payload := auth.GetPayloadFromJwt[P](*token, a.Auth.GetSecret())
+	return payload
+}
+
+func (a *Api[P]) GetClientPayloadX(ctx context.Context) *P {
+	payload := a.GetClientPayload(ctx)
+
+	if payload == nil {
+		exception.NotAccess("Не удалось получить доступ")
+	}
+
 	return payload
 }
